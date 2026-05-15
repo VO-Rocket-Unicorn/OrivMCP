f43333f600cfda520215d149926296b01ccd4e8f
KingReevan <reevandmellO@gmail.com>
2026-05-15 16:22:48 +0530

    feat: :sparkles: Update settings to include allowed hosts and origins; refactor app initialization

--- oriv_mcp/config/settings.py
+++ oriv_mcp/config/settings.py
@@ -1,3 +1,4 @@
+from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -7,19 +8,31 @@ class Settings(BaseSettings):
     Reads from environment variables with prefix: ORIV_MCP_
     """
 
-    # ---- server ----
-    host: str = "127.0.0.1"
-    port: int = 8000
-    workers: int = 1
-    timeout_keep_alive: int = 5
-
-    # ---- environment ----
-    env: str = "dev"  # dev | prod
-
     model_config = SettingsConfigDict(
         env_prefix="ORIV_MCP_",
         env_file=".env",
         extra="ignore",
+    )
+
+    # ---- server ----
+    host: str = Field(default="127.0.01", description="Host to run the server on")
+    port: int = Field(default=8000, description="Port to run the server on")
+    workers: int = Field(
+        default=1, description="Number of worker processes for handling requests"
+    )
+    timeout_keep_alive: int = Field(
+        default=5,
+        description="Number of seconds to wait for the next request on a Keep-Alive connection",
+    )
+
+    # ---- environment ----
+    env: str = Field(default="development", description="Application environment")
+
+    allowed_hosts: list[str] = Field(
+        default_factory=list, description="List of allowed hosts for CORS"
+    )
+    allowed_origins: list[str] = Field(
+        default_factory=list, description="List of allowed origins for CORS"
     )
 
 
--- oriv_mcp/server/app.py
+++ oriv_mcp/server/app.py
@@ -1,6 +1,11 @@
+import uvicorn
 from mcp.server.fastmcp import FastMCP
 from mcp.server.transport_security import (
     TransportSecuritySettings,
+)
+
+from oriv_mcp.server.bootstrap import (
+    register_all_tools,
 )
 
 mcp_app = FastMCP(
@@ -20,3 +25,21 @@ mcp_app = FastMCP(
         ],
     ),
 )
+
+
+# Register once
+register_all_tools()
+
+
+app = mcp_app.streamable_http_app()
+
+
+if __name__ == "__main__":
+
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=8000,
+        # proxy_headers=True,
+        forwarded_allow_ips="*",
+    )
