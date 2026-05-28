acb57d964c67b417b0cc087809e180a90607570c
rahuljsaliaan <rahuljsaliaan@gmail.com>
2026-05-28 10:13:48 +0530

    enhance: :star: Remove unnecessary imports.

--- oriv_mcp/server/lifespan.py
+++ oriv_mcp/server/lifespan.py
@@ -1,14 +1,14 @@
 from contextlib import asynccontextmanager
 from oriv_mcp.config.http_config import http_client
 
+
 @asynccontextmanager
 async def lifespan(app):
     try:
         # Code to run on startup
         print("Starting up the application...")
         yield
-    finally:  
-        http_client.aclose()  # Close the HTTP client on shutdown
+    finally:
+        await http_client.aclose()  # Close the HTTP client on shutdown
         # Code to run on shutdown
         print("Shutting down the application...")
-        
